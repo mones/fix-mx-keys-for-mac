16d33339c172a7bfa66327a5e8a86c568ede3394
Ricardo Mones <ricardo@mones.org>
2020-12-18 01:43:21 +0100

    Remap keys under X
    Swapping assignment of scan-codes 49 (0x31) and 94 (0x5e) seems enough.

---
+++ fix-mx-keys-for-mac.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xmodmap -e "keycode  49 = less greater less greater bar brokenbar bar"
+xmodmap -e "keycode  94 = masculine ordfeminine masculine ordfeminine backslash backslash backslash"
